86e0071620d4304ea0bf4af63b4bfd4a935d3f70
Kelly Sandeson <kellysandeson@gmail.com>
2019-04-30 17:40:09 -0600

    Fix end needed on 14

--- lib/turns.rb
+++ lib/turns.rb
@@ -4,12 +4,14 @@ attr_reader :guess, :card
     @guess = guess
     @card = card
   end
+
   def correct?
     if @guess == true
       puts "Correct"
     else
       puts "False"
     end
+  end
   def feedback
     if correct? == true
       puts "Correct!"
