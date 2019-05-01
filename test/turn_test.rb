d601ab162069cdc8495e358319c62986521dfaa8
Kelly Sandeson <kellysandeson@gmail.com>
2019-04-30 22:21:37 -0600

    Create successful turn test code

---
+++ test/turn_test.rb
@@ -0,0 +1,47 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/card'
+require './lib/turn'
+
+class TurnTest < Minitest::Test
+
+  def test_it_exists
+    card = Card.new("What is the capital of Colorado?", "Denver", :Geography)
+    turn = Turn.new("Denver", card)
+
+    assert_instance_of Turn, turn
+  end
+
+  def test_it_has_a_guess
+    card = Card.new("What is the capital of Colorado?", "Denver", :Geography)
+    turn = Turn.new("Denver", card)
+
+    assert_equal "Denver", turn.guess
+  end
+
+  def test_it_has_a_card
+    card = Card.new("What is the capital of Colorado?", "Denver", :Geography)
+    turn = Turn.new("Denver", card)
+
+    assert_equal card, turn.card
+
+  end
+
+  def test_it_is_correct?
+    card = Card.new("What is the capital of Colorado?", "Denver", :Geography)
+    turn = Turn.new("Denver", card)
+
+    assert_equal true, turn.correct?
+  end
+
+  def test_it_gives_correct_feedback
+    card = Card.new("What is the capital of Colorado?", "Denver", :Geography)
+    turn_1 = Turn.new("Denver", card)
+    turn_2 = Turn.new("Jacksonville", card)
+
+    assert_equal "Correct!", turn_1.feedback
+    assert_equal "Incorrect!", turn_2.feedback
+
+  end
+end
