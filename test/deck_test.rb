d559ec070072e24f96809d5b7ab2f6dfe575aad7
Kelly Sandeson <kellysandeson@gmail.com>
2019-05-05 21:35:04 -0600

    Final Commit

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -25,4 +25,13 @@ class DeckTest < Minitest::Test
 
     assert_equal [card_2], deck.cards_in_category(:STEM)
   end
+
+  def test_count
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    deck = Deck.new([card, card_2])
+
+    assert_equal 2, deck.count
+  end
 end
